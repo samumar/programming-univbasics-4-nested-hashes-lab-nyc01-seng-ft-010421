7eb9250651d7a94caf953fe42bc19b8351be5d43
samumar43 <samumar43@gmail.com>
2020-10-18 03:19:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,6 @@ def hopper
     }
 
 end
-hopper([:grace_hopper])
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -39,7 +38,6 @@ def alan_kay_is_known_for
     }
 
 end
-alan_kay_is_known_for([:alan_kay][:known_for])
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -58,7 +56,6 @@ def dennis_ritchies_language
       }
     }
 end
-dennis_ritchies_language([:dennis_ritchie][:known_for])
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -85,8 +82,6 @@ def adding_matz
     }
 
 end
-adding_matz([:yukihiro_matsumoto][:known_for] = "Ruby")
-adding_matz([:yukihiro_matsumoto][:languages] = ["LISP", "C"])
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -109,7 +104,6 @@ def changing_alan
     }
 
 end
-changing_alan([:alan_kay][:known_for] = "GUI")
 
 def adding_to_dennis
   # add "Assembly" to Dennis Ritchie's languages array
@@ -132,4 +126,3 @@ def adding_to_dennis
     }
 
 end
-adding_to_dennis([:dennis_ritchie][:languages] = ["C", "Assembly"])
