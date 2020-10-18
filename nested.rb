23ff3c9364412b9810de44792ee2c699c0805235
samumar43 <samumar43@gmail.com>
2020-10-18 03:29:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -14,18 +14,9 @@ def alan_kay_is_known_for
 	
 	programmer_hash = 
  		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
+      :alan_kay => 
+        :known_for => "Object Orientation"
+      
     }
 
 end
