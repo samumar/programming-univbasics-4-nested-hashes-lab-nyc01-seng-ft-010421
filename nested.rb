c59898e716db183d733ed15353d529a279c83738
samumar43 <samumar43@gmail.com>
2020-10-18 03:46:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -75,5 +75,11 @@ def adding_to_dennis
   # return the entire updated hash
 
 	programmer_hash = 
-	"C"
+ 		{
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C", "Assembly"]
+      }
+    }
+
 end
