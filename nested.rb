37686934de3b1a2fcb4888e23087f54bcf608b29
samumar43 <samumar43@gmail.com>
2020-10-18 03:24:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -5,17 +5,9 @@ def hopper
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
       }
     }
-
+    
 end
 
 def alan_kay_is_known_for
