fbf29fe1f9becee096e9273758a94edd4a81ec99
samumar43 <samumar43@gmail.com>
2020-10-18 03:35:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -24,14 +24,6 @@ end
 def dennis_ritchies_language
 	programmer_hash = 
  		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
